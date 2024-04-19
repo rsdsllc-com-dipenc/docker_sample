6f891b092d02d7be4ac6f48013444407aa999d32
Dipen Chauhan <dipenc@rsdsllc.com>
2024-04-19 09:59:04 -0700

    added body

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
+  has_rich_text :body
 end
